2b7f7b925393932edfc0e43d1ecb9f96a9644850
areeva <89791802+areeva@users.noreply.github.com>
2023-07-08 13:37:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+make_it_right_song="";
+life_goes_on_song="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+scoreleftWrist = 0;
+song_name = "";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function preload(){
+    life_goes_on_song = loadSound("life-goes-on.mp3");
+    make_it_right_song = loadSound("make-it-right.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_name = life_goes_on_song.isPlaying();
+    console.log(song_name);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        make_it_right_song.stop();
+        if(song_name == false){
+           life_goes_on_song.play();
+        }
+        else{
+            console.log("Song Name: life goes on");
+            document.getElementById("song_id").innerHTML = "Song Name: life goes on ";
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
